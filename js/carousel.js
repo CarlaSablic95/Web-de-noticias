30b14db5ea1e51e1b9a4e7247e516b0ced1b89b9
CarlaSablic95 <carlasablic95@gmail.com>
2024-02-07 13:26:15 -0300

    Maquetación de barra de navegación, slider con noticias

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+        // Opciones de configuración
+        type: 'slide',
+        perPage: 1,
+        arrows: false,
+        // autoplay: true,
+        // interval: 2000
+        // Más opciones...
+    }).mount();
+});
